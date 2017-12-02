a7e5b3042c50727131406424e4156ee21fe4f22b
kenseii <oasisagano@gmail.com>
2017-12-02 12:27:57 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { PropTypes, Component } from 'react';
+import { AppRegistry,Text,StatusBar,View } from 'react-native';
+import Header from './src/components/header';
+
+const App = () => (
+
+    <Header/>
+    );
+
+
+// skip this line if using Create React Native App
+//AppRegistry.registerComponent('albums', () => App);
+//
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React, { PropTypes, Component } from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Header = () => <Text>Albums</Text>;
+
+
+export default Header;
