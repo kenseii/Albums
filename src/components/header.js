b488c7a5856d6e212c5692e7b8dfd7d40c752600
kenseii <oasisagano@gmail.com>
2017-12-03 15:07:28 +0200

    Header Edits

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,34 @@
 /* eslint-disable no-unused-vars */
 import React, { PropTypes, Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View,StatusBar } from 'react-native';
 
-const Header = () => <Text>Albums</Text>;
+const Header = () => {
+    const { textStyle, viewStyles } = styles;
+
+
+    return (<View style={viewStyles}>
+        <StatusBar hidden={true} />
+        <Text style={textStyle}>Albums</Text></View>);
+    };
+
+const styles = {
+    viewStyles:{
+        backgroundColor:'#ececec',
+        alignItems:'center',
+        justifyContent:'center',
+        height:60,
+        paddingTop:10,
+        shadowColor:'#000',
+        shadowOffset:{width:0, height:2},
+        shadowOpacity:0.2,
+        elevation:2,
+        position:'relative'
+    },
+    textStyle: {
+        fontSize: 25,
+
+    }
+};
 
 
 export default Header;
